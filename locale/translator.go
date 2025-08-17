fd5759590c89167aa3cb963d1eaf56395c66a5dc
dgf <deep@dagnu.de>
2025-08-17 17:51:06 +0100

    lint locale handler

--- locale/translator.go
+++ locale/translator.go
@@ -15,6 +15,8 @@ var localeFS embed.FS
 
 var bundle *i18n.Bundle
 
+var messageByID map[string]*i18n.Message
+
 type Translator interface {
 	Translate(messageID string) string
 	TranslateData(messageID string, data map[string]string) string
@@ -24,49 +26,48 @@ type translator struct {
 	localizer *i18n.Localizer
 }
 
-var messageByID map[string]*i18n.Message
+var messages = [...]*i18n.Message{
+	{ID: "bad_request_path_param", Other: "Bad Request, invalid path param '{{.param}}' value '{{.value}}'"},
+	{ID: "client_error", Other: "Client Error"},
+	{ID: "conflict_task_update", Other: "The task update has failed due to an editing conflict. Please try the update again."},
+	{ID: "database_error", Other: "Database Error {{.message}}"},
+	{ID: "internal_server_error", Other: "Internal Server Error"},
+	{ID: "not_found_path", Other: "Not Found '{{.method}} {{.path}}'"},
+	{ID: "not_found_task", Other: "Task '{{.id}}' not found."},
+	{ID: "ok_task_created", Other: "Task '{{.id}}' created."},
+	{ID: "ok_task_updated", Other: "Task '{{.id}}' updated."},
+	{ID: "order_ascending", Other: "ascending"},
+	{ID: "order_descending", Other: "descending"},
+	{ID: "page_number", Other: "page"},
+	{ID: "page_size", Other: "size"},
+	{ID: "page_title", Other: "My Tasks"},
+	{ID: "task_add", Other: "add"},
+	{ID: "task_back", Other: "back"},
+	{ID: "task_cancel", Other: "cancel"},
+	{ID: "task_confirm_delete", Other: "Are you sure?"},
+	{ID: "task_count", Other: "count"},
+	{ID: "task_create", Other: "create"},
+	{ID: "task_created_at", Other: "create at"},
+	{ID: "task_creating", Other: "creating"},
+	{ID: "task_description", Other: "description"},
+	{ID: "task_due_date", Other: "due date"},
+	{ID: "task_edit", Other: "edit"},
+	{ID: "task_order", Other: "order"},
+	{ID: "task_results", Other: "results"},
+	{ID: "task_save", Other: "save"},
+	{ID: "task_saving", Other: "saving"},
+	{ID: "task_sort", Other: "sort"},
+	{ID: "task_subject", Other: "subject"},
+	{ID: "task_subject_filter", Other: "subject filter"},
+	{ID: "tasks_loading", Other: "loading"},
+}
 
 func init() {
 	bundle = i18n.NewBundle(language.English)
 	bundle.RegisterUnmarshalFunc("toml", toml.Unmarshal)
-	if _, err := bundle.LoadMessageFileFS(localeFS, "active.de.toml"); err != nil {
+	_, err := bundle.LoadMessageFileFS(localeFS, "active.de.toml")
+	if err != nil {
 		log.Error("bundle message load failed", err)
-	}
-
-	messages := [...]*i18n.Message{
-		{ID: "bad_request_path_param", Other: "Bad Request, invalid path param '{{.param}}' value '{{.value}}'"},
-		{ID: "client_error", Other: "Client Error"},
-		{ID: "conflict_task_update", Other: "The task update has failed due to an editing conflict. Please try the update again."},
-		{ID: "database_error", Other: "Database Error {{.message}}"},
-		{ID: "internal_server_error", Other: "Internal Server Error"},
-		{ID: "not_found_path", Other: "Not Found '{{.method}} {{.path}}'"},
-		{ID: "not_found_task", Other: "Task '{{.id}}' not found."},
-		{ID: "ok_task_created", Other: "Task '{{.id}}' created."},
-		{ID: "ok_task_updated", Other: "Task '{{.id}}' updated."},
-		{ID: "order_ascending", Other: "ascending"},
-		{ID: "order_descending", Other: "descending"},
-		{ID: "page_number", Other: "page"},
-		{ID: "page_size", Other: "size"},
-		{ID: "page_title", Other: "My Tasks"},
-		{ID: "task_add", Other: "add"},
-		{ID: "task_back", Other: "back"},
-		{ID: "task_cancel", Other: "cancel"},
-		{ID: "task_confirm_delete", Other: "Are you sure?"},
-		{ID: "task_count", Other: "count"},
-		{ID: "task_create", Other: "create"},
-		{ID: "task_created_at", Other: "create at"},
-		{ID: "task_creating", Other: "creating"},
-		{ID: "task_description", Other: "description"},
-		{ID: "task_due_date", Other: "due date"},
-		{ID: "task_edit", Other: "edit"},
-		{ID: "task_order", Other: "order"},
-		{ID: "task_results", Other: "results"},
-		{ID: "task_save", Other: "save"},
-		{ID: "task_saving", Other: "saving"},
-		{ID: "task_sort", Other: "sort"},
-		{ID: "task_subject", Other: "subject"},
-		{ID: "task_subject_filter", Other: "subject filter"},
-		{ID: "tasks_loading", Other: "loading"},
 	}
 
 	messageByID = make(map[string]*i18n.Message, len(messages))
@@ -82,25 +83,37 @@ func RequestTranslator(lang language.Tag) Translator {
 }
 
 func (m *translator) Translate(messageID string) string {
-	if message, ok := messageByID[messageID]; !ok {
+	message, ok := messageByID[messageID]
+	if !ok {
 		log.Warn("unknown translation message", "messageID", messageID)
+
 		return messageID
-	} else if translation, err := m.localizer.Localize(&i18n.LocalizeConfig{DefaultMessage: message}); err != nil {
-		log.Warn(fmt.Sprintf("translation error: %v", err), "messageID", messageID)
-		return messageID
-	} else {
-		return translation
 	}
+
+	translation, err := m.localizer.Localize(&i18n.LocalizeConfig{DefaultMessage: message})
+	if err != nil {
+		log.Warn(fmt.Sprintf("translation error: %v", err), "messageID", messageID)
+
+		return messageID
+	}
+
+	return translation
 }
 
 func (m *translator) TranslateData(messageID string, data map[string]string) string {
-	if message, ok := messageByID[messageID]; !ok {
+	message, ok := messageByID[messageID]
+	if !ok {
 		log.Warn("unknown translation message", "messageID", messageID)
+
 		return messageID
-	} else if translation, err := m.localizer.Localize(&i18n.LocalizeConfig{DefaultMessage: message, TemplateData: data}); err != nil {
-		log.Warn(fmt.Sprintf("translation error: %v", err), "messageID", messageID)
-		return messageID
-	} else {
-		return translation
 	}
+
+	translation, err := m.localizer.Localize(&i18n.LocalizeConfig{DefaultMessage: message, TemplateData: data})
+	if err != nil {
+		log.Warn(fmt.Sprintf("translation error: %v", err), "messageID", messageID)
+
+		return messageID
+	}
+
+	return translation
 }
