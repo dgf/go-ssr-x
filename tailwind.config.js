efc70c6395192c4f8b7722f3cbf882e59acea62d
dgf <deep@dagnu.de>
2025-08-17 15:53:53 +0100

    fix Tailwind config content path prop

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./view/**/*.templ"],
+  content: ["./web/**/*.templ"],
   theme: {
     container: {
       center: true,
