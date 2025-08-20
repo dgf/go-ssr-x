aaa49d99ae1e5e521227a9b41caeb41813a4ad8c
dgf <deep@dagnu.de>
2025-08-20 22:55:41 +0100

    add CLI delete call

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"time"
 
 	"github.com/alecthomas/kong"
 	"github.com/charmbracelet/glamour"
@@ -35,8 +36,8 @@ func runWithStorage(globals *Globals, run func(context.Context, io.Writer, entit
 }
 
 type PageTasksCmd struct {
-	Page   int `default:"1" help:"page number to show."`
-	Size   int `default:"10" help:"page size to show."`
+	Page   int `default:"1" help:"Page number to show."`
+	Size   int `default:"10" help:"Page size to show."`
 	Sort   entity.TaskSort
 	Order  entity.SortOrder
 	Filter string `help:"Match subject filter."`
@@ -70,7 +71,7 @@ func (cmd *PageTasksCmd) Run(globals *Globals) error {
 			q.Filter)
 
 		fmt.Fprintf(w, " # \t %s \t %s \t %s \n\n",
-			"UUID                                ",
+			"ID                                  ",
 			locale.Translate(ctx, "task_due_date"),
 			locale.Translate(ctx, "task_subject"))
 
@@ -86,19 +87,40 @@ func (cmd *PageTasksCmd) Run(globals *Globals) error {
 	})
 }
 
+type AddTaskCmd struct {
+	Subject string `arg:"" required:""`
+}
+
+func (cmd *AddTaskCmd) Run(globals *Globals) error {
+	return runWithStorage(globals, func(ctx context.Context, w io.Writer, storage entity.Storage) error {
+		data := entity.TaskData{
+			DueDate: time.Now().Add(14 * 24 * time.Hour), // 2 weeks
+			Subject: cmd.Subject,
+		}
+
+		id, err := storage.AddTask(ctx, data)
+		if err != nil {
+			return err
+		}
+		fmt.Fprintln(w, locale.TranslateData(ctx, "ok_task_created", map[string]string{"id": id.String()}))
+
+		return nil
+	})
+}
+
 type ShowTaskCmd struct {
-	UUID uuid.UUID `arg:"" required:"" help:"uuid of task to show"`
+	ID uuid.UUID `arg:"" required:"" help:"ID of task to show."`
 }
 
 func (cmd *ShowTaskCmd) Run(globals *Globals) error {
 	return runWithStorage(globals, func(ctx context.Context, w io.Writer, storage entity.Storage) error {
-		task, found, err := storage.Task(ctx, cmd.UUID)
+		task, found, err := storage.Task(ctx, cmd.ID)
 		if err != nil {
 			return err
 		}
 
 		if !found {
-			return errors.New(locale.TranslateData(ctx, "not_found_task", map[string]string{"id": cmd.UUID.String()}))
+			return errors.New(locale.TranslateData(ctx, "not_found_task", map[string]string{"id": cmd.ID.String()}))
 		}
 
 		fmt.Fprintf(w, "\n %s:\t%s\n",
@@ -117,11 +139,23 @@ func (cmd *ShowTaskCmd) Run(globals *Globals) error {
 	})
 }
 
+type DeleteTaskCmd struct {
+	ID uuid.UUID `arg:"" required:"" help:"ID of task to delete."`
+}
+
+func (cmd *DeleteTaskCmd) Run(globals *Globals) error {
+	return runWithStorage(globals, func(ctx context.Context, w io.Writer, storage entity.Storage) error {
+		return storage.DeleteTask(ctx, cmd.ID)
+	})
+}
+
 var CLI struct {
 	File string `default:".tasks.sqlite" help:"File based storage backend path (your data)."`
 
-	Tasks PageTasksCmd `cmd:"" default:"1" help:"List tasks."`
-	Show  ShowTaskCmd  `cmd:"" help:"Show task."`
+	List   PageTasksCmd  `cmd:"" default:"1" help:"List tasks."`
+	Show   ShowTaskCmd   `cmd:"" help:"Show task."`
+	Add    AddTaskCmd    `cmd:"" help:"Add task."`
+	Delete DeleteTaskCmd `cmd:"" aliases:"del" help:"Delete a task."`
 }
 
 func main() {
